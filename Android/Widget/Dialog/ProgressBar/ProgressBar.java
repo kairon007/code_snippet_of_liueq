698766513e407e9bc700cae15062489d2a900598
liueq <liueqmaaya@gmail.com>
2015-07-20 11:19:52 +0800

    增加了ProgressBar

---
+++ Android/Widget/Dialog/ProgressBar/ProgressBar.java
@@ -0,0 +1,10 @@
+/**
+ * Title: ProgressBar
+ * Tag: ProgressBar
+ * Update: 2015/07/20
+ * Description:
+ *  1. 最简单的用法就是让Progressbar出现和消失
+ */
+
+ProgressBar _progress;
+_progress.setVisibility(View.VISIBLE);
