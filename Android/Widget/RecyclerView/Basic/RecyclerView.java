c215a2b55337be4e88c984cdab73196051d5b988
liueq <liueqmaaya@gmail.com>
2015-07-28 18:25:51 +0800

    增加了SearchView的使用方法 增加了RecyclerView的使用方法

--- Android/Widget/RecyclerView/Basic/RecyclerView.java
+++ Android/Widget/RecyclerView/Basic/RecyclerView.java
@@ -1,6 +1,6 @@
 /**
  * Title: RecyclerView
- * Update: 2015/07/13
+ * Update: 2015/07/28
  * Description:
  *  1. 关于LayoutMananger，常见的有3中LinearLayoutManager, GridLayoutManager, StaggeredGridLayoutManager
  *  2. 调用RecyclerView默认动画，通过notify不同的类型，RecyclerView会自动显示动画
@@ -10,6 +10,7 @@
  *	notifyItemRangeChanged()
  *	notifyItemRangeInserted()
  *	notifyItemRangeRemoved()
+ *  3. 增加了滑动监听
  */
 
 @Override
@@ -25,78 +26,101 @@ protected void onCreate(Bundle savedInstanceState) {
     List<String> list = Arrays.asList(strs);
     RecyclerListAdapter adapter = new RecyclerListAdapter(this, list);
 
-    recyclerView.setAdapter(adapter);		//直接设置Adapter即可
+    //Recycler的ScrollListener
+    recyclerView.addOnScrollListener(new OnScrollListener() {
 
+	private boolean scrollUp = true;
+
+	@Override
+	public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+	    super.onScrollStateChanged(recyclerView, newState);
+	    //newState的3种状态，只有在这3种状态切换才会触发
+	    //1. SCROLL_STATE_DRAGGING 触摸进行中，正在滑动
+	    //2. SCROLL_STATE_IDLE 没有滑动
+	    //3. SCROLL_STATE_SETTLING 惯性滑动中，没有触摸
+	}
+
+	@Override
+	public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+	    super.onScrolled(recyclerView, dx, dy);
+
+	    //dx和dy表示的是滑动的距离，由开始触摸的位置减去结束触摸的位置
+	    //常用的判断方式是，当dy>0的时候，认为是向上滑动，反之向下
+	}
+    }
+
+
+    recyclerView.setAdapter(adapter);		//直接设置Adapter即可
 }
 
-/**
- * 关于RecyclerViewAdapter的写法
- * 这里继承了RecyclerView.Adapter，泛型需要使用ViewHolder，可以在Adapter中定义一个内部类
- * RecyclerView 的item点击事件，应该在ViewHolder中处理，对ViewHolder中的View进行监听
- */
-public class RecyclerListAdapter extends RecyclerView.Adapter<RecyclerListAdapter.ViewHolder> {
+	/**
+	 * 关于RecyclerViewAdapter的写法
+	 * 这里继承了RecyclerView.Adapter，泛型需要使用ViewHolder，可以在Adapter中定义一个内部类
+	 * RecyclerView 的item点击事件，应该在ViewHolder中处理，对ViewHolder中的View进行监听
+	 */
+	public class RecyclerListAdapter extends RecyclerView.Adapter<RecyclerListAdapter.ViewHolder> {
 
-    private Context mContext;
-    private List<String> mList;
+	    private Context mContext;
+	    private List<String> mList;
 
-    //传入的参数依旧是Context和list
-    public RecyclerListAdapter(Context context, List<String> list){
-	this.mList = list;
-	mContext = context;
-    }
+	    //传入的参数依旧是Context和list
+	    public RecyclerListAdapter(Context context, List<String> list){
+		this.mList = list;
+		mContext = context;
+	    }
 
-    /** 
-     * 创建ViewHolder，从布局文件中获取ViewHode的布局，并且新建一个Holder，作为返回值
-     * 注意第二个参数，根据viewType来生成不同的Holder对象
-     */
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-	View v = (View) LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
-	ViewHolder viewHolder = new ViewHolder(v);
-	return viewHolder;
-    }
-
-    //对于Holder对象的赋值，在此进行，这里和一般的ListView中的Adapter不同，ViewHolder的生成和对其的赋值是在两个不同的方法中进行
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int i) {
-	viewHolder.mTextView.setText(mList.get(i));
-	viewHolder.setOnClickListener(new View.setOnClickListener(){
+	    /** 
+	     * 创建ViewHolder，从布局文件中获取ViewHode的布局，并且新建一个Holder，作为返回值
+	     * 注意第二个参数，根据viewType来生成不同的Holder对象
+	     */
 	    @Override
-	    public void onClick(View v){
-		//TODO 点击事件
+	    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+		View v = (View) LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
+		ViewHolder viewHolder = new ViewHolder(v);
+		return viewHolder;
+	    }
+
+	    //对于Holder对象的赋值，在此进行，这里和一般的ListView中的Adapter不同，ViewHolder的生成和对其的赋值是在两个不同的方法中进行
+	    @Override
+	    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+		viewHolder.mTextView.setText(mList.get(i));
+		viewHolder.setOnClickListener(new View.setOnClickListener(){
+		    @Override
+		    public void onClick(View v){
+			//TODO 点击事件
+		    }
+		}
+
+	    }
+
+	    @Override
+	    public int getItemCount() {
+		return mList.size();
+	    }
+
+	    /**
+	     * 获取Item的类型，可以使用固定的，也可以根据List中的对象类型来返回不同类型
+	     * 这里的返回值会用来在onCreateViewHolder中
+	     */
+	    @Override
+	    public int getItemViewType(int position){
+		int type = mList.get(position).getType(); //这里演示的是item获取类型
+		return type;
+	    }
+
+	    /**
+	     * 定义一个内部类作为ViewHolder，需要继承RecyclerView.ViewHolder
+	     * 另外一定值得注意的地方是静态内部类可以节省内存
+	     * ViewHolder可以有多个
+	     */
+	    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+		public final TextView mTextView;
+
+		public ViewHolder(View itemView) {
+		    super(itemView);
+		    mTextView = (TextView) itemView.findViewById(R.id.tv);
+
+		}
 	    }
 	}
-
-    }
-
-    @Override
-    public int getItemCount() {
-	return mList.size();
-    }
-
-    /**
-     * 获取Item的类型，可以使用固定的，也可以根据List中的对象类型来返回不同类型
-     * 这里的返回值会用来在onCreateViewHolder中
-     */
-    @Override
-    public int getItemViewType(int position){
-	int type = mList.get(position).getType(); //这里演示的是item获取类型
-	return type;
-    }
-
-    /**
-     * 定义一个内部类作为ViewHolder，需要继承RecyclerView.ViewHolder
-     * 另外一定值得注意的地方是静态内部类可以节省内存
-     * ViewHolder可以有多个
-     */
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-
-	public final TextView mTextView;
-
-	public ViewHolder(View itemView) {
-	    super(itemView);
-	    mTextView = (TextView) itemView.findViewById(R.id.tv);
-
-	}
-    }
-}
--- Android/Widget/Support_Library/SearchView/SearchView.java
+++ Android/Widget/Support_Library/SearchView/SearchView.java
@@ -1,9 +1,10 @@
 /**
  * Title: Toolbar的搜索栏, SearchView
  * Tag: SearchView, Search
- * Update: 2015/07/17
+ * Update: 2015/07/27
  * Description:
  *  1. 主要有两种方式，一种是点击键盘上的搜索按钮，跳转到SearchResult的Activity；另一种是不跳转，监听SearchView的Text变化，实时的过滤掉当前的List中的结果
+ *  2. 增加了左侧返回按钮出现的条件,实际上，最简单的方式是在xml文件中，定义app:showAsAction="always|collapseActionView"，这样就会自动出现返回按钮，并且布局也正确,但是需要自定义icon，不能使用@android的文件，否则会出现4.x的搜索图标
  */
 
 /**
@@ -35,7 +36,7 @@ if (searchView != null) {
 	    return false;
 	}
     });
-    
+
     /**
      * 当焦点从SearchView移开的时候触发
      */
@@ -49,12 +50,24 @@ if (searchView != null) {
     });
 
     /**
+     * 当Searchview还未展开之前，点击search图标触发
+     */
+    searchView.setOnSearchClickListener(new OnClickListener() {
+	@Override
+	public void onClick(View v) {
+	    getSupportActionBar().setDisplayHomeAsUpEnabled(true); //当不想给Toolbar添加默认的返回按钮，但是由需要给SearchView添加的时候，动态的设置
+	}
+    });
+
+    /**
      * 当关闭SearchView的时候触发
      */
     searchView.setOnCloseListener(new SearchView.OnCloseListener() {
 	@Override
 	public boolean onClose() {
 	    presenter.loadData();
+	    getSupportActionBar().setDisplayHomeAsUpEnabled(false); //当不想给Toolbar添加默认的返回按钮，但是由需要给SearchView添加的时候，动态的设置
+
 	    return false;
 	}
     });
